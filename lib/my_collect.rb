71bc743b3982b199616608377161c59c8415f97c
Daniel Sullivan <dansullivan19@gmail.com>
2019-04-06 23:38:16 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,17 @@
 def my_collect(array)
+  collection = []
   i = 0 
   while i < array.length
-  yield array[i]
+  collection.push(yield(array[i]))
   i += 1 
   end
+  collection
 end
 
 # array = ['ruby', 'javascript', 'python', 'objective-c']
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
-# my_collect(array) { |lang| puts lang.upcase }
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
-my_collect(array) { |name| name.split(" ").first }
+# # my_collect(array) { |lang| puts lang.upcase }
+
+# my_collect(array) { |name| name.split(" ").first }
