669a021f43a607215959e6f4a6f526ff3554b3f4
Daniel Sullivan <dansullivan19@gmail.com>
2019-04-06 23:06:33 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  i = 0 
+  while i < array.length
+  yield array[i]
+  i += 1 
+  end
+end
 
+# array = ['ruby', 'javascript', 'python', 'objective-c']
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+# my_collect(array) { |lang| puts lang.upcase }
+
+my_collect(array) { |name| name.split(" ").first }
